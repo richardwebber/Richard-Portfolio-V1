230dde7c95c15c5dbd539210683b6e360f607407
Richard Webber <rikki.webber@gmail.com>
2024-02-15 00:17:43 -0700

    Starting on the parallax sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import './app.scss'
 import Navbar from './components/navbar/Navbar'
 import { Hero } from './components/hero/Hero'
-import { Test } from './Test'
+import { Parallax } from './components/parallax/Parallax'
+// import { Test } from './Test'
 
 const App = () => {
 
@@ -11,9 +12,9 @@ const App = () => {
       <Navbar />
       <Hero />
       </section>
-      <section id='Services'>Parallax</section>
+      <section id='Services'><Parallax type='services'/></section>
       <section>Services</section>
-      <section id='Portfolio'>Parallax</section>
+      <section id='Portfolio'><Parallax type='portfolio'/></section>
       <section>Portfolio1</section>
       <section>Portfolio2</section>
       <section>Portfolio3</section>
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -18,7 +18,7 @@ const textVariants = {
     opacity: 0,
     y: 10,
     transition: {
-        duration: 2,
+        duration: 2.2,
         repeat: Infinity,
     }
   }
