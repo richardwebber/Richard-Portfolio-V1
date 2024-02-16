98a871a3e614a00401550541dcd850e079c13d60
Richard Webber <rikki.webber@gmail.com>
2024-02-15 23:06:22 -0700

    Sstruggling to fix the x direction. Motion keeps breaking

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,18 +1,36 @@
+import { useRef } from "react";
 import "./parallax.scss";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 export const Parallax = ({ type }) => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  const yText = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
+  const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const xBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
 
   return (
     <div
       className="parallax"
+      ref={ref}
       style={{
-        background:type==='services' ? `linear-gradient(180deg, #000000, #a3a3c2)` : `linear-gradient(180deg, #99b3e6, #ebf0fa)`
+        background:
+          type === "services"
+            ? `linear-gradient(180deg, #000000, #a3a3c2)`
+            : `linear-gradient(180deg, #99b3e6, #ebf0fa)`,
       }}
     >
-      <h1>{type === "services" ? "What We Do?" : "What We Did?"}</h1>
-      <div className="mountains"></div>
-      <div className="planets"></div>
-      <div className="stars"></div>
+      <motion.h1 style={{y:yBg}}>
+        {type === "services" ? "[skills]" : "[projects]"}
+      </motion.h1>
+      <motion.div className="mountains"></motion.div>
+      <motion.div className="planets"></motion.div>
+      <motion.div style={{y:yBg}} className="stars"></motion.div>
     </div>
   );
 };
