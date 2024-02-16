9ee3f98f1da104982aa659c5f0ca82007d6b617e
Richard Webber <rikki.webber@gmail.com>
2024-02-16 02:02:14 -0700

    Fixed bug, found out that brave cant do certain motions

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,12 +1,26 @@
+import { useRef } from "react"
 import "./parallax.scss"
+import { motion, useScroll, useTransform } from 'framer-motion'
 
 const Parallax = () => {
+
+  const ref = useRef()
+
+  const {scrollYProgress} = useScroll({
+    target:ref,
+    offset: ["start start", "end start"]
+  })
+
+  const yText = useTransform(scrollYProgress, [0,1], ["0%", "500%"])
+  const yBg = useTransform(scrollYProgress, [0,1], ["0%", "100%"])
+
+
   return (
-    <div className="parallax" style={{background: "linear-gradient(180deg,  #000000, #3939ac, #cc99ff, #f2ccff"}}>
-      <h1>'[projects]'</h1>
-      <div className="mountains"></div>
-      <div className="planets"></div>
-      <div className="stars"></div>
+    <div className="parallax" ref={ref} style={{background: "linear-gradient(180deg,  #000000, #3939ac, #cc99ff, #f2ccff"}}>
+      <motion.h1 style={{y:yText}}>'[projects]'</motion.h1>
+      <motion.div className="mountains"></motion.div>
+      <motion.div style={{y:yBg}} className="planets"></motion.div>
+      <motion.div style={{x:yBg}} className="stars"></motion.div>
     </div>
   )
 }
