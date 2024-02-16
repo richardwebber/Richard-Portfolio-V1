df01c622bdb1922e049f99e1c69c7533bb117213
Richard Webber <rikki.webber@gmail.com>
2024-02-16 01:01:13 -0700

    Fixed the bug... Changed plan and removed some code to add better more logical code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./app.scss";
 import Navbar from "./components/navbar/Navbar";
 import { Hero } from "./components/hero/Hero";
-import { Parallax } from "./components/parallax/Parallax";
+import Parallax from "./components/parallax/Parallax.jsx";
 // import { Test } from './Test'
 
 const App = () => {
@@ -11,12 +11,8 @@ const App = () => {
         <Navbar />
         <Hero />
       </section>
-      <section id="Services">
-        <Parallax type="services" />
-      </section>
-      <section>Services</section>
       <section id="Portfolio">
-        <Parallax type="portfolio" />
+        <Parallax />
       </section>
       <section>Portfolio1</section>
       <section>Portfolio2</section>
--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,36 +1,14 @@
-import { useRef } from "react";
-import "./parallax.scss";
-import { motion, useScroll, useTransform } from "framer-motion";
+import "./parallax.scss"
 
-export const Parallax = ({ type }) => {
-  const ref = useRef();
-
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["start start", "end start"],
-  });
-
-  const yText = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
-  const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
-  const xBg = useTransform(scrollYProgress, [0, 1], ["0%", "200%"]);
-
+const Parallax = () => {
   return (
-    <div
-      className="parallax"
-      ref={ref}
-      style={{
-        background:
-          type === "services"
-            ? `linear-gradient(180deg, #000000, #a3a3c2)`
-            : `linear-gradient(180deg, #99b3e6, #ebf0fa)`,
-      }}
-    >
-      <motion.h1 style={{y:yText}}>
-        {type === "services" ? "[skills]" : "[projects]"}
-      </motion.h1>
-      <motion.div className="mountains"></motion.div>
-      <motion.div style={{y:yBg}} className="planets"></motion.div>
-      <motion.div style={{x:yBg}} className="stars"></motion.div>
+    <div className="parallax" style={{background: "linear-gradient(180deg,  #000000, #3939ac, #cc99ff, #f2ccff"}}>
+      <h1>'[projects]'</h1>
+      <div className="mountains"></div>
+      <div className="planets"></div>
+      <div className="stars"></div>
     </div>
-  );
-};
+  )
+}
+
+export default Parallax
