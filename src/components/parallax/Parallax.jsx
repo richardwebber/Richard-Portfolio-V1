89b3318bb4334eb2ffd1aa1a57d7e113faa60af8
Richard Webber <rikki.webber@gmail.com>
2024-02-16 16:42:42 -0700

    Happy with Parallax.jsx. Onto the next

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -11,16 +11,23 @@ const Parallax = () => {
     offset: ["start start", "end start"]
   })
 
-  const yText = useTransform(scrollYProgress, [0,1], ["0%", "500%"])
+  const yText = useTransform(scrollYProgress, [0,1], ["0%", "430%"])
   const yBg = useTransform(scrollYProgress, [0,1], ["0%", "500%"])
-  const yBg2 = useTransform(scrollYProgress, [0,1], ["0%", "100%"])
+  const logoBg = useTransform(scrollYProgress, [0,1], ["0%", "-150%"])
+  const yBg2 = useTransform(scrollYProgress, [0,1], ["0%", "70%"])
 
 
   return (
-    <div className="parallax" ref={ref} style={{background: "linear-gradient(180deg,  #000000, #3939ac, #cc99ff, #f2ccff"}}>
+    <div className="parallax" ref={ref} style={{background: "linear-gradient(180deg,  #000000, #4C4C4C, #A3A3A3, #F4F4F4"}}>
       <motion.h1 style={{y:yText}}>'[projects]'</motion.h1>
       <motion.div className="mountains"></motion.div>
-      {/* <motion.div style={{y:yBg}} className="planets"></motion.div> */}
+      <motion.div style={{y:logoBg}} className="react-logo" ></motion.div>
+      <motion.div style={{y:logoBg}} className="js-logo" ></motion.div>
+      <motion.div style={{y:logoBg}} className="ts-logo" ></motion.div>
+      <motion.div style={{y:logoBg}} className="html-logo" ></motion.div>
+      <motion.div style={{y:logoBg}} className="css-logo" ></motion.div>
+      <motion.div style={{y:logoBg}} className="nodejs-logo" ></motion.div>
+      <motion.div style={{y:logoBg}} className="postgresql-logo" ></motion.div>
       <motion.div style={{y:yBg2}} className="stars"></motion.div>
     </div>
   )
