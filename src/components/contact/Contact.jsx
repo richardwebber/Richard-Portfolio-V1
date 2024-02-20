fd911a826dc5ba6b8fc22945107f62f2cc044bee
Richard Webber <rikki.webber@gmail.com>
2024-02-20 16:40:57 -0700

    Save point before fixing form. SVG messed it up... oops

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,30 +1,41 @@
 import "./contact.scss";
-import {motion} from 'framer-motion'
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
+
+const variants = {
+  initial: {
+    y: 500,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.1,
+    },
+  },
+};
 
 const Contact = () => {
-  const variants = {
-    initial: {
-      y: 500,
-      opacity: 0,
-    },
-    animate: {
-      y: 0,
-      opacity: 1,
-      transition: {
-        duration: 0.5,
-        staggerChildren: 0.1,
-      },
-    },
-  };
+  const ref = useRef();
+
+  const isInView = useInView(ref, { margin: "-100px" });
 
   return (
-    <motion.div className="contact" variants={variants} initial="initial" whileInView="animate">
+    <motion.div
+      ref={ref}
+      className="contact"
+      variants={variants}
+      initial="initial"
+      whileInView="animate"
+    >
       <motion.div className="textContainer" variants={variants}>
         <motion.h1 variants={variants}>Lets work together</motion.h1>
         <motion.div className="item" variants={variants}>
           <h2>Mail</h2>
           <span>hello@react.dev</span>
-        </motion.div >
+        </motion.div>
         <motion.div className="item" variants={variants}>
           <h2>Address</h2>
           <span>Salt Lake City, Utah</span>
@@ -35,12 +46,38 @@ const Contact = () => {
         </motion.div>
       </motion.div>
       <div className="formContainer">
-        <form>
+        <motion.div
+          className="phoneSvg"
+          initial={{ opacity: 1 }}
+          whileInView={{ opacity: 0 }}
+          transition={{ delay: 3, duration: 1 }}
+        >
+          <svg width="450px" height="450px" viewBox="0 0 32.666 32.666">
+            <motion.path
+              strokeWidth={0.2}
+              fill="none"
+              initial={{ pathLength: 0 }}
+              animate={isInView && { pathLength: 1 }}
+              transition={{ duration: 3 }}
+              d="M62.9891,2.5618c-0.0765-0.5779-0.6611-0.9805-1.2299-0.8401L7.4043,15.2065c-0.3535,0.0879-0.6318,0.3608-0.7256,0.7129     
+            s0.0112,0.7275,0.2744,0.9795l13.9343,13.3583l-2.7649,17.1495c-0.1079,0.6712,0.4969,1.2576,1.1582,1.1445l18.0805-3.1324     
+            l17.1832,9.6988c0.1523,0.0859,0.3218,0.1289,0.4917,0.1289c0.1523,0,0.3047-0.0347,0.4453-0.1045     
+            c0.2969-0.1475,0.5015-0.4331,0.5459-0.7617l6.9639-51.5542C63.0031,2.7372,63.0007,2.6487,62.9891,2.5618z M9.647,16.7109     
+            L56.8914,4.9902L22.2545,28.7978L9.647,16.7109z M36.9146,43.4663l-16.5942,2.875l2.4995-15.5054L58.8633,6.0615L36.9146,43.4663     
+            z M54.2427,52.6504l-15.3231-8.6492l21.4231-36.509L54.2427,52.6504z"
+            />
+          </svg>
+        </motion.div>
+        <motion.form
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ delay: 4, duration: 1 }}
+        >
           <input type="text" required placeholder="Name" />
           <input type="email" required placeholder="Email" />
           <textarea rows={8} placeholder="" />
           <button>Submit</button>
-        </form>
+        </motion.form>
       </div>
     </motion.div>
   );
