cd60827ad69f1286928573c4d42f0c908e392fcd
Richard Webber <rikki.webber@gmail.com>
2024-03-04 11:44:08 -0700

    We be committing

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,22 +7,28 @@ const items = [
     id: 1,
     title: "Where To?",
     img: "WTEventsDT.png",
+    imgDesc:"Browse events",
     desc: "Where To? is a CRUD social media application built using the PERN stack. I used React JS, Node.js, SASS/CSS, Bootstrap/Material UI (component libraries), Zustand (global state management), Google Maps API and Axios for the front end. For the back end I used PostgreSQL, Express (server & sessions) and Bcrypt (login encryption).",
     desc2: "Where to was created with the intention to provide a solution for the perpetual question of what to do in your free time. Whether you're seeking adventure, socializing with others, or exploring new hobbies, Where To? is your go-to companion. I believe in the power of community and shared experiences, and I've built a platform that reflects this philosophy. With Where To? you can create, share, post and like events with your friends. Think instagram but instead you are sharing events with your community. ",
+    link: "https://github.com/project-where-to/where-to",
   },
   {
     id: 2,
     title: "RAW.house Collective",
+    imgDesc:"Browse events",
     img: "https://wallpapercave.com/wp/wp11678597.jpg",
     desc: "RAW.house Collective is an ecommerce CRUD application using the PERN stack. I used React JS, Node.js, SASS/CSS, React Bootstrap (component library), Redux (global state management) and Axios for the front end. On the back end I used PostgreSQL and Express (server & sessions).",
     desc2: "I created RAW.house Collective because I wanted to create a clothing website where the target audience was individuals who need tough, stylish clothing that compliments their hobby. The target  audience are individuals who enjoy drifting cars, skateboarding and the aesthetic/ energy that comes from those communities. ",
+    link: "https://github.com/richardwebber/DevMountain-Personal-Project",
   },
   {
     id: 3,
     title: "Customer Admin APP",
+    imgDesc:"Browse events",
     img: "https://w0.peakpx.com/wallpaper/207/486/HD-wallpaper-laguna-beach-usa-california-dawn-night-lights-scenery-relaxing-mood-nature.jpg",
     desc: "Where To is an app that brings people together by creating, likeing, saving and sharing events!",
     desc2: "Lorem ipsum",
+    link: "https://reports.growthboundmarketing.com/",
   },
 ];
 
@@ -42,12 +48,13 @@ const Single = ({ item }) => {
         <div className="wrapper">
           <div className="imageContainer" ref={ref}>
             <img src={item.img} alt="" />
+            {/* <h5>{item.imgDesc}</h5> */}
           </div>
           <motion.div className="textContainer" style={{y}}>
             <h2>{item.title}</h2>
             <p>{item.desc}</p>
             <p>{item.desc2}</p>
-            <button>DEMO</button>
+            <button onClick={() => window.location.href = item.link}>DEMO</button>
           </motion.div>
         </div>
       </div>
